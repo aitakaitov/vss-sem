507fbf5d0733e4cb80d9cc6142fb0c72fa8e139b
Vojtěch Bartička <aitakaitov@gmail.com>
2022-10-26 16:11:01 +0200

    custom metrics because of weird sklearn behaviour

--- metrics.py
+++ metrics.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 import numpy as np
 
 
@@ -6,18 +6,17 @@ def evaluate_predictions(y_score, y_true) -> dict:
     y_score_greater = np.array(y_score)[:, 1]
     y_pred = np.argmax(y_score, axis=1)
     results = {
-        'accuracy': accuracy_score(y_true, y_pred),
         'precision': 1 - precision_score(y_true, y_pred),
         'recall': 1 - recall_score(y_true, y_pred),
-        'f1': f1_score(y_true, y_pred, average='micro'),
+        'f1': 1- f1_score(y_true, y_pred),
         'auc': roc_auc_score(y_true, y_score_greater, average='micro'),
-        'fa': fa_score(y_pred, y_true)
+        'fa': fa_score(y_true, y_pred)
     }
 
     return results
 
 
-def fa_score(y_pred, y_true):
+def fa_score(y_true, y_pred):
     fp = 0.0
     fn = 0.0
     tn = 0.0
@@ -30,3 +29,42 @@ def fa_score(y_pred, y_true):
             tn += 1
 
     return fn / (fp + tn)
+
+
+def f1_score(y_true, y_pred):
+    fp = 0.0
+    fn = 0.0
+    tp = 0.0
+    for pred, true in zip(y_pred, y_true):
+        if pred == 1 and true == 0:
+            fp += 1
+        elif pred == 0 and true == 1:
+            fn += 1
+        elif pred == 1 and true == 1:
+            tp += 1
+
+    return (2 * tp) / (2 * tp + fp + fn)
+
+
+def recall_score(y_true, y_pred):
+    fn = 0.0
+    tp = 0.0
+    for pred, true in zip(y_pred, y_true):
+        if pred == 0 and true == 1:
+            fn += 1
+        elif pred == 1 and true == 1:
+            tp += 1
+
+    return tp / (tp + fn)
+
+
+def precision_score(y_true, y_pred):
+    fp = 0.0
+    tp = 0.0
+    for pred, true in zip(y_pred, y_true):
+        if pred == 1 and true == 0:
+            fp += 1
+        elif pred == 1 and true == 1:
+            tp += 1
+
+    return tp / (tp + fp)
